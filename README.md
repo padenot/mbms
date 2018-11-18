8a334b3f47388ff67756e0261f8d17655c8ccf48
Paul Adenot <paul@paul.cx>
2018-11-18 19:21:46 +0100

    initial shell working with 2 apps

---
+++ README.md
@@ -0,0 +1,11 @@
+#mbms
+
+shell to run multiple bela/monome apps on one monome and one bela, having all of
+them running concurrently.
+
+# License
+
+Either of:
+
+* Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
---
+++ src/main.rs
@@ -0,0 +1,182 @@
+extern crate log;
+extern crate mlr_rs;
+extern crate mds;
+extern crate bela;
+extern crate pretty_env_logger;
+extern crate mbms_traits;
+extern crate monome;
+
+use std::thread;
+use std::time::Duration;
+
+use bela::*;
+use mlr_rs::{MLR, MLRRenderer};
+use mds::{MDS, MDSRenderer};
+use mbms_traits::*;
+use monome::*;
+
+struct Control {
+    controllers: Vec<Box<InstrumentControl>>,
+    current: usize,
+    monome: Monome
+}
+
+impl Control {
+  fn new(monome: Monome) -> Control {
+      Control {
+          controllers: Vec::new(),
+          current: 0,
+          monome
+      }
+  }
+  fn push(&mut self, controller: Box<InstrumentControl>) {
+      self.controllers.push(controller);
+  }
+}
+
+struct Render {
+    renderers: Vec<Box<InstrumentRenderer>>
+}
+
+impl Render {
+    fn new() -> Render {
+        Render {
+            renderers: Vec::new()
+        }
+    }
+    fn push(&mut self, renderer: Box<InstrumentRenderer>) {
+        self.renderers.push(renderer);
+    }
+}
+
+struct MonomeTask<F> {
+    callback: F,
+    args: Control
+}
+
+impl<F> Auxiliary for MonomeTask<F>
+where F: FnMut(&mut Control),
+      for<'r> F: FnMut(&'r mut Control)
+{
+    type Args = Control;
+
+    fn destructure(&mut self) -> (&mut FnMut(&mut Control), &mut Self::Args) {
+        let MonomeTask {
+            callback,
+            args,
+        } = self;
+
+        (callback, args)
+    }
+}
+
+type BelaApp<'a> = Bela<AppData<'a, Render>>;
+
+fn go() -> Result<(), bela::error::Error> {
+    println!("loading samples & decoding...");
+
+    let (mlr, mlr_renderer) = MLR::new(128., 44100);
+    let (mds, mds_renderer) = MDS::new(16, 8, 128.);
+    let monome = Monome::new("/prefix".to_string()).unwrap();
+
+    let mut control = Control::new(monome);
+    control.push(Box::new(mlr));
+    control.push(Box::new(mds));
+
+    let mut render = Render::new();
+    render.push(Box::new(mlr_renderer));
+    render.push(Box::new(mds_renderer));
+
+    let mut monome_task = MonomeTask {
+        callback: |control: &mut Control| {
+            let mut grid = [0 as u8; 128];
+            let mut last_grid = [0 as u8; 128];
+            let mut ggrid = [false; 128];
+            let monome = &mut control.monome;
+            let controllers = &mut control.controllers;
+            loop {
+                match monome.poll() {
+                    Some(e) => {
+                        match e {
+                            MonomeEvent::GridKey { x, y, direction: _ } => {
+                                if y == 0 && x < 8 {
+                                    control.current = x as usize;
+                                } else {
+                                    controllers[control.current].input(e);
+                                }
+                            },
+                            _ => { }
+                        }
+                    }
+                    _ => { }
+                }
+                for i in controllers.iter_mut() {
+                    i.main_thread_work();
+                }
+                controllers[control.current].render(&mut grid);
+
+                // light up the current instrument
+                grid[control.current] = 15;
+
+                let mut equal = true;
+                for i in 0..128 {
+                    if grid[i] != last_grid[i] {
+                        equal = false;
+                        break;
+                    }
+                }
+
+                for i in 0..128 {
+                    if grid[i] != 0 {
+                        ggrid[i] = true;
+                    }
+                }
+
+                if (!equal) {
+                    monome.set_all(&ggrid.to_vec());
+                    last_grid = grid;
+                }
+
+                grid.iter_mut().map(|x| *x = 0).count();
+                ggrid.iter_mut().map(|x| *x = false).count();
+
+                thread::sleep(Duration::from_millis(32));
+            }
+        },
+        args: control
+    };
+
+    let mut setup = |_context: &mut Context, _user_data: &mut Render| -> Result<(), error::Error> {
+        println!("Setting up");
+        let task = BelaApp::create_auxiliary_task(&mut monome_task, 10, "monome_task");
+        BelaApp::schedule_auxiliary_task(&task)?;
+        println!("ok");
+        Ok(())
+    };
+
+    let mut cleanup = |_context: &mut Context, _user_data: &mut Render| {
+        println!("Cleaning up");
+    };
+
+    let mut render_func = |context: &mut Context, renderers: &mut Render| {
+        let insts = &mut renderers.renderers;
+        for r in insts.iter_mut() {
+            r.render(context);
+        }
+    };
+
+    let user_data = AppData::new(render, &mut render_func, Some(&mut setup), Some(&mut cleanup));
+
+    let mut settings = InitSettings::default();
+
+    Bela::new(user_data).run(&mut settings)
+}
+
+fn main() {
+    pretty_env_logger::init();
+
+    match go() {
+        Ok(_) => { println!("??"); }
+        Err(_) => { println!("!!"); }
+    }
+}
